b4495bb0a5ee64134e5c9da7dd7d72c90aa4a486
Marcelo Junior <marcelo.jr63@gmail.com>
2018-11-27 20:08:30 -0200

    Adding plugin axios into chat#vue

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -2,6 +2,7 @@
 //= require vendor/bootstrap.min
 //= require vendor/vue
 //= require vendor/hoy3lrg
+//= require vendor/axios.min
 
 //= require ./chat/sidepanel
 //= require ./chat/content-chat
@@ -11,7 +12,6 @@ var app = new Vue({
   el: '#app',
   props: ['contact_info'],
   data: {
-    user: {},
     contacts: []
   },
   template: '#frame-template',
@@ -26,30 +26,14 @@ var app = new Vue({
     }
   },
   created: function() {
-    this.user = {
-      name: 'Javier Boulsonarro',
-      image_url: 'http://emilcarlsson.se/assets/mikeross.png'
-    };
-    this.contacts = [
-      {
-        user_id: 1,
-        active: 'active',
-        status_user: 'online',
-        img_url: 'http://emilcarlsson.se/assets/harveyspecter.png',
-        name: 'Paulim Guedeson',
-        preview: 'Vai mudar isso ai mesmo?',
-        send_by_user: false
-      },
-      {
-        user_id: 2,
-        active: '',
-        status_user: 'offline',
-        img_url: 'http://emilcarlsson.se/assets/harveyspecter.png',
-        name: 'Lulinha Free',
-        preview: 'Tem que se f*** e acabou!',
-        send_by_user: true
-      }
-    ];
-    this.current_contact = this.contacts[0]
+    chat = this
+    axios.get('/chat_apis')
+      .then(function(response) {
+        chat.contacts = response.data
+        chat.current_contact = chat.contacts[0]
+      })
+      .catch(function(error) {
+        alert("Ops... Deu ruim!\nVeja com o Marcelo Jr. o que rolou :)")
+      })
   }
 });
